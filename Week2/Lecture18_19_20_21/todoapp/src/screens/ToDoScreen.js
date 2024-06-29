b83e50814ea9cdf7f1d52672d5251b4824c331ed
Rohit kumar <rohitranjanrrsingh@gmail.com>
2024-06-29 20:38:57 +0530

    add input fields

--- Week2/Lecture18_19_20_21/todoapp/src/screens/ToDoScreen.js
+++ Week2/Lecture18_19_20_21/todoapp/src/screens/ToDoScreen.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import Task from '../components/Task';
+import AddTask from '../components/AddTask';
 
 export const ToDoScreen = () => {
     const [taskList, setTaskList] = useState([]);
+    const [addTask,setAddTask]=useState(false);
+
+    
+
     function increment() {
         try{
             setTaskList([
@@ -11,26 +16,29 @@ export const ToDoScreen = () => {
                     title: "Title",
                     description: "This is description my",
                     date:Date.now()
-                    
                 },
             ]);
         }catch(error){
-            console.log("Error in setTaskList: ",error)
+            console.log("Error in setTaskList: ",error);
         }
     }
     return (
         <div>
             <h1 className="ui heading center">TO DO List</h1>
             <div className='btn-addTask'>
-                <button onClick={increment}
-                    className="ui secondary button">Add Task</button>
+                <button onClick={()=>setAddTask(!addTask)}
+                    className="ui secondary button">Add Task
+                </button>
             </div>
-
+            <div>
+                { addTask?<AddTask increment={increment}/>:<div></div>}
+            </div>
             <section>
                 <div className='ui cards cardContainer'>
                     {taskList.map((task,index) => (<Task task={task} key={index}/>))}
                 </div>
             </section>
+            
         </div>
     )
 }
