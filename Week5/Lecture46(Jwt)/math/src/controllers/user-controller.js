20be7e51a42c3a985da4cbd95c0bf09c8fc52b80
Rohit kumar <rohitranjanrrsingh@gmail.com>
2024-07-18 11:19:46 +0530

    Middleware and JWT

--- Week5/Lecture46(Jwt)/math/src/controllers/user-controller.js
+++ Week5/Lecture46(Jwt)/math/src/controllers/user-controller.js
@@ -1,30 +1,38 @@
-const User=require('../models/User');
+const User = require('../models/User');
 
-//add new user
-exports.addNewUser=async(req,res)=>{
-    try{
-        const {name,email,age,password} = req.body;
-        const user = new User(name,email,age,password);
+// Add new user
+exports.addNewUser = async (req, res) => {
+    try {
+        const { name, email, age, password } = req.body;
+        const user = new User({ name, email, age, password });
         await user.save();
-        res.status(200).send(user);
-
-    }catch(error){
-        console.log(error);
-        return res.status(500).send({message: "SignUp failed"});
+        res.status(200).send({
+            success:true,
+            message:"New User registerd successfully",
+            User:user
+        });
+    } catch (error) {
+        console.log("Error in addNewUser controller: " + error.message);
+        return res.status(500).send({ 
+            success:false,
+            message: "SignUp failed"
+        });
     }
 }
 
-
-//login
-exports.loginUser=async(req,res)=>{
-    try{
-        const {email,password} = req.body;
-        const user=await User.findByEmailAndPasswordForAuth(email,password);
-        console.info(`User with email:${email} successfully login!`);
-        return res.status(200).send(user);
-
-    }catch(error){
-        console.log(error);
-        return res.status(500).send({message: "Login failed"});
+// Login
+exports.loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findByEmailAndPasswordForAuth(email, password);
+        const token = await user.generateToken();
+        console.info(`User with email: ${email} successfully login!`);
+        return res.status(200).send({ 
+            message: `User login with email: ${email} successfull`,
+            user, token 
+        });
+    } catch (error) {
+        console.log("Error in loginUser controller: " + error.message);
+        return res.status(500).send({ message: "Login failed" });
     }
 }
